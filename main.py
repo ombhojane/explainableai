b24819d34ef8a410bbeb5c6080539e08dc6ecfba
PreetSharma-12 <mr.bhardwaz14@gmail.com>
2024-10-20 23:25:00 +0530

    removed import of numpy library
    I removed the import of unnecessary import of the numpy library

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
